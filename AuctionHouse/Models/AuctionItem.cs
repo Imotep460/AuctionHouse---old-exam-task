9561eeaa96b7de749345f883d1925f8fd896f6e8
Hasse Augustinussen <hasse_augu@hotmail.dk>
2018-12-03 11:03:34 +0100

    creation of solution and created AuctionItem.cs class

---
+++ AuctionHouse/Models/AuctionItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuctionHouse.Models
+{
+    public class AuctionItem
+    {
+        public long AuctionItemId { get; set; }
+        public string ItemDescription { get; set; }
+        public decimal RatingPrice{ get; set; }
+        public decimal BidPrice { get; set; }
+        public string BidCustomName { get; set; }
+        public string CustomerPhone { get; set; }
+        public DateTime BidTimeStamp { get; set; }
+    }
+}
